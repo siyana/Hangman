75b27e6c76ec95df396a4a59b6f75051136fb727
Siyana Slavova <siyana.slavova@SiyanaSlavova.local>
2014-02-14 17:21:46 +0200

    Add console menu

---
+++ console_menu.rb
@@ -0,0 +1,85 @@
+module ConsoleMenu
+  extend self
+  require "./load_dictionary"
+  
+  def show_start_menu
+    show_options_for_menu @menu_options
+    get_users_choice_for_menu
+  end
+  
+  private
+  
+  MENU_AGANST_PC = "Against PC"
+  MENU_MULTYPLAYER = "Multyplayer"
+  MENU_OPTIONS = "Options"
+  MENU_SCORES = "Scores"
+  
+  @menu_options = [MENU_AGANST_PC, MENU_MULTYPLAYER, MENU_OPTIONS, MENU_SCORES]
+  
+  #show menus methods
+  
+  def get_users_choice_for_menu
+      puts "Please, enter the number of your choice:"
+      @initial_choice = gets.strip.to_i
+      case @initial_choice
+          when 1, 2 then choose_dictionary
+          when 3 then show_options_menu
+          when 4 then show_scores
+      end
+  end
+  
+  def choose_dictionary
+    puts "Do you wanna see categories or word's length? Enter your choice:"
+    show_options_for_menu ["By category", "By length"]
+    choice = gets.strip.to_i
+    case choice
+      when 1
+       choose_dictionary_by_category
+      when 2
+        choose_dictionary_by_length
+    end
+  end
+  
+  def show_options_menu
+    p "3"
+  end
+  
+  def show_scores
+    p "4"
+  end
+  
+  #choose dictionary methods
+  
+  def choose_dictionary_by_category
+    puts "Please, choose a dictionary:"
+    all_categories = LoadDictionary.get_all_categories
+    show_options_for_menu all_categories
+    choice = gets.strip.to_i
+    load_words_from_dictionary LoadDictionary.make_dictionary_by_category all_categories[choice - 1]
+  end
+  
+  def choose_dictionary_by_length
+    puts "Please, choose a dictionary:"
+    all_categories = LoadDictionary.get_all_word_lengths
+    show_options_for_menu all_categories
+    choice = gets.strip.to_i
+    load_words_from_dictionary LoadDictionary.make_dictionary_by_word_length all_categories[choice - 1]
+  end
+  
+  def load_words_from_dictionary(dictionary)
+    case @initial_choice
+      when 1 then LoadDictionary.random_word_from_dictionary dictionary
+      when 2 then LoadDictionary.choose_word_from_dictionary dictionary
+    end
+  end
+  
+  #help methods
+  def show_options_for_menu(menu)
+    menu.each_with_index do |option, index|
+      puts "#{index + 1}. #{option}"
+    end
+  end
+
+end
+
+ConsoleMenu.show_start_menu
